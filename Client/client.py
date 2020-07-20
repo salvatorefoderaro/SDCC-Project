acbe2758727297013285267738f4056cfdfdbefa
Salvatore <salvatore.foderaro@gmail.com>
2020-07-20 11:52:15 +0200

    #11 - Fix integrazione modulo (Issues #3)
    1. Risolto sia per il client che per il Server. La vecchia versione, oltre che qua nei commit su Git, è presente nelle cartelle "Old code"

---
+++ Client/client.py
@@ -0,0 +1,103 @@
+from flask import Flask, request #import main Flask class and request object
+import requests
+import os
+import socket
+import re
+import sys
+import time
+import threading
+import socket
+import time
+import requests
+import json
+from threading import Thread
+
+MINE_IP_ADDRESS = ""
+CLUSTER_IP_ADDRESS = ""
+SEARCH_INTERVAL = 5
+BCAST_IP = '239.255.255.250'
+BCAST_PORT = 10000
+port = 9001
+protocol = "blockchain"
+networkid = "main1111"
+
+def getMineIPAddress():
+    global MINE_IP_ADDRESS
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    MINE_IP_ADDRESS = IP
+
+def getClusterIPAddress():
+        global CLUSTER_IP_ADDRESS
+        '''
+        broadcast SSDP DISCOVER message to LAN network
+        filter our protocal and add to network
+        '''
+        while True:
+            try:
+                SSDP_DISCOVER = ('M-SEARCH * HTTP/1.1\r\n' +
+                            'HOST: 239.255.255.250:1900\r\n' +
+                            'MAN: "ssdp:discover"\r\n' +
+                            'MX: 1\r\n' +
+                            'ST: ssdp:all\r\n' +
+                            '\r\n')
+                LOCATION_REGEX = re.compile("LOCATION: {}_{}://[ ]*(.+)\r\n".format(protocol, networkid), re.IGNORECASE)
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock.sendto(SSDP_DISCOVER.encode('ASCII'), (BCAST_IP, BCAST_PORT))
+                sock.settimeout(3)
+                while True:
+                    data, addr = sock.recvfrom(1024)
+                    print("Risposta ricevuta!")
+                    CLUSTER_IP_ADDRESS = addr[0].split('\'')[0]
+                    return
+            except:
+                sock.close()
+
+
+app = Flask(__name__) #create the Flask app
+
+# Router per la get per controllare lo stato del dispositivo
+@app.route('/checkStatus', methods=['GET'])
+def query_example():
+    return "I'm alive"
+
+# Route per modificare la configurazione a runtime, magari tramite la dashboard
+@app.route('/editConfig', methods=['GET'])
+def editConfig():
+    configFile = open("config.json", "r")
+    json_object = json.load(configFile)
+    
+    if (request.args.get("name") is not None):
+        json_object["name"] = request.args.get("name")
+    if (requests.arg.get("groupName") is not None):
+        json_object["groupName"] = request.args.get("groupName")
+
+    json.dump(json_object, configFile)
+    configFile.close()
+
+def doSomeStuff():
+    getClusterIPAddress()
+    getMineIPAddress()
+    with open('config.json') as config_file:
+        data = json.load(config_file)
+    
+    while (True):
+        dictToSend = {'id':data['id'],  'temperatura': 10, 'umidita': 11}
+        try:
+            res = requests.post('http://'+CLUSTER_IP_ADDRESS+':5000/sendDataToCluster', json=dictToSend)
+        except requests.exceptions.RequestException as e:  # This is the correct syntax
+            print("ricevuto errore")
+            getClusterIPAddress()
+        time.sleep(60)
+
+if __name__ == '__main__':
+    thread = Thread(target = doSomeStuff)
+    thread.start()
+    app.run(host='0.0.0.0', debug=False, port=7000) #run app in debug mode on port 5000
--- Server/proxy_server.py
+++ Server/proxy_server.py
@@ -1,10 +1,15 @@
 from flask import Flask, request #import main Flask class and request object
 import requests
 import subprocess
+from ssdp import Server
+from ssdp import gen_logger
 
 app = Flask(__name__) #create the Flask app
 
 SERVICE_EXTERNAL_IP = ""
+
+logger = gen_logger('sample')
+
 
 def getServiceExternalIP():
     global SERVICE_EXTERNAL_IP
@@ -31,10 +36,13 @@ def query_example():
 def jsonexample():
     dictToSend = {'id':request.json['id'], 'temperatura':request.json['temperatura'], 'umidita':request.json['umidita']}
     try:
-        res = requests.post('http://' + SERVICE_EXTERNAL_ip + ':30006/collectData', json=dictToSend)
+        res = requests.post('http://' + SERVICE_EXTERNAL_IP + ':30006/collectData', json=dictToSend)
+        return res.text
     except requests.exceptions.RequestException as e:  # This is the correct syntax
         return e
-    return res.text
 
 if __name__ == '__main__':
+    getServiceExternalIP()
+    upnpServer = Server(9001, 'blockchain', 'main1111')
+    upnpServer.start()
     app.run(host='0.0.0.0', debug=True, port=5000, threaded=True) #run app in debug mode on port 5000
