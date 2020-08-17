d6bbcad4b4dd15678d3322e6860fc69ed8e37336
Gimmi <aa@gmail.com>
2020-08-17 13:09:31 +0200

    Pre-Merge (1)

--- Server/instantiate_cluster.sh
+++ Server/instantiate_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minikube start --vm-driver=docker
+minikube start
 minikube addons enable storage-provisioner
 eval $(minikube docker-env)
 cd check_devices_status && docker build -t checkdevicestatus:v1 .
